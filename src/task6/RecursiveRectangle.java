6b31d807335972afb1127cdb6e56d4bbe61ae13d
merkulovvs <mervic81@gmail.com>
2017-11-29 23:27:11 +0200

    task6 not solved

--- src/task6/RecursiveRectangle.java
+++ src/task6/RecursiveRectangle.java
@@ -1,5 +1,27 @@
 package task6;
 
+import java.util.Scanner;
+
 public class RecursiveRectangle {
-    
+
+    static void recursiveRectangle(int a, int b){
+
+
+        System.out.print("+ ");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the rectangle height: ");
+        int height = scanner.nextInt();
+        System.out.print("Please enter the rectangle width: ");
+        int width = scanner.nextInt();
+        System.out.println(height + " " + width);
+
+        recursiveRectangle(height, width);
+
+
+    }
 }
