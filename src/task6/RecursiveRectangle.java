1db2bf89143f8597d928fe464178fa366d0170cd
merkulovvs <mervic81@gmail.com>
2017-11-30 18:02:29 +0200

    completed task6

--- src/task6/RecursiveRectangle.java
+++ src/task6/RecursiveRectangle.java
@@ -6,9 +6,17 @@ public class RecursiveRectangle {
 
     static void recursiveRectangle(int a, int b){
 
+        if(b>0){
+            a*=b;
+            b=-b;
+        }
 
-        System.out.print("+ ");
-
+        System.out.print("+");
+        a--;
+        if(a%b==0)
+            System.out.println();
+        if(a!=0)
+            recursiveRectangle(a, b);
     }
 
     public static void main(String[] args) {
